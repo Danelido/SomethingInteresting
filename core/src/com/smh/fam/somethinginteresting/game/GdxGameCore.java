0bb0b2ce69dcf45b1429ee84e1a7c02b46b6536a
Alexander Danliden <AlexanderAlsvold@hotmail.se>
2017-03-24 22:44:33 +0100

    Testar om allt funkar som smööööör

--- core/src/com/smh/fam/somethinginteresting/game/GdxGameCore.java
+++ core/src/com/smh/fam/somethinginteresting/game/GdxGameCore.java
@@ -14,11 +14,14 @@ public class GdxGameCore extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0.5f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
