03de0f1b8bc0ea715a98b291024d2800df1d1567
Alexander Danliden <AlexanderAlsvold@hotmail.se>
2017-03-24 22:33:30 +0100

    AlltIhop
    Hela projektet

---
+++ desktop/src/com/smh/fam/somethinginteresting/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smh.fam.somethinginteresting.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smh.fam.somethinginteresting.game.GdxGameCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGameCore(), config);
+	}
+}
