8c4522efad1f251098003fec695b67cb79679896
Axel Wickman <axelwickm@gmail.com>
2017-03-24 23:52:05 +0100

    Started implementing Box2D

---
+++ core/src/com/smh/fam/somethinginteresting/game/Box2D_Simulator.java
@@ -0,0 +1,64 @@
+package com.smh.fam.somethinginteresting.game;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by Axel on 2017-03-24.
+ */
+
+public class Box2D_Simulator {
+    private float simulatedTime = 0.0f;
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
+
+    private final int VELOCITY_ITERATIONS = 2;
+    private final int POSITION_ITERATIONS = 2;
+
+    public Box2D_Simulator(){
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer = new Box2DDebugRenderer();
+
+        createBox();
+    }
+
+    public void simulate(float deltaT){
+        deltaT = Math.min(deltaT, 0.25f); // To avoid stepping too far on slow devices
+        simulatedTime += deltaT;
+
+        world.step(deltaT,VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+    }
+
+    public void createBox(){
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(100, 300);
+
+        Body body = world.createBody(bodyDef);
+
+        PolygonShape boxShape = new PolygonShape();
+        boxShape.setAsBox(30.0f, 30.0f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = boxShape;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        boxShape.dispose();
+    }
+
+    public float getSimulatedTime(){
+        return simulatedTime;
+    }
+}
--- core/src/com/smh/fam/somethinginteresting/game/GdxGameCore.java
+++ core/src/com/smh/fam/somethinginteresting/game/GdxGameCore.java
@@ -5,16 +5,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Box2D;
 
 public class GdxGameCore extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	
+	Box2D_Simulator box2D_simulator;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 
+		box2D_simulator = new Box2D_Simulator();
 	}
 
 	@Override
@@ -24,6 +28,8 @@ public class GdxGameCore extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
+		box2D_simulator.simulate(0.01f);
 	}
 	
 	@Override
